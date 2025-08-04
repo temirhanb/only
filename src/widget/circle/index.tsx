e55b37f4643d57d4925beb64fd527bd99232474a
temirhanb <temirhan023@gmail.com>
2025-08-04 21:31:11 +0500

    -desktop animation complete

--- src/widget/circle/index.tsx
+++ src/widget/circle/index.tsx
@@ -1,26 +1,42 @@
-import React, {useEffect, useRef} from "react";
+import React, {useRef, useState} from "react";
 import gsap from "gsap";
 import {useMainPageState} from "../../shared/state/mainPageState";
 import "./styles/index.scss";
+import {useGSAP} from "@gsap/react";
 
 export const CircleComponent: React.FC = () => {
-  const {currentSlide} = useMainPageState(state => state);
+  const {currentSlide, data} = useMainPageState(state => state);
 
   const circleRef = useRef(null); // Create a ref to target the circle element
+  const pointsRef = useRef(null); // Create a ref to target the circle element
 
-  useEffect(() => {
-    console.log("update");
+  const [watchPoint, setWatchPoint] = useState(false);
+  const [rotation, setRotation] = useState(0);
+  useGSAP(() => {
+    setWatchPoint(true);
     gsap.to(circleRef.current, {
       x: currentSlide,
-      rotation: 90,
-      duration: 2,
+      rotation: rotation,
+      duration: 1,
+      onComplete: () => setWatchPoint(false)
     });
+    setRotation(prevState => prevState + 120);
   }, [currentSlide]);
+  console.log(data.length);
   return (
-    <div ref={circleRef} className={"container__circle"}>
-      <div className={"container__circle-slide"}>
+    <div className={"container__circle"}>
+      <div ref={circleRef} className={"circle"}>
+        <div ref={pointsRef} className={`points__container ${watchPoint ? "hidden" : ""}`}>
+          {data.map((_, index) => (
+            <div key={index} className={`circle__point ${"index" + index}`}/>
+          ))}
+        </div>
+
+      </div>
+      <div className={`container__circle-slide ${watchPoint ? "hidden" : ""}`}>
         <span>{currentSlide}</span>
       </div>
     </div>
+
   );
 };
